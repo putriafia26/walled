13f5151aa58bda3bb54683679e98d5914e1eb3a5
putri afia <putriafianurfallahi@gmail.com>
2024-12-09 13:42:01 +0700

    feat: create header and add state to change tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css'
+import { useState } from "react"
+import logo from "./assets/WhatsApp Image 2024-12-09 at 11.23.54.jpeg"
+import NavItems from "./components/NavItems"
 function App() {
+  const [activeTab, setActiveTab] = useState("dashboard")
+  const menu  = [
+    {
+      title: "Dashboard",
+      link: "#"
+    },
+    {
+      title: "Transfer",
+      link: "#"
+    },
+    {
+      title: "Topup",
+      link: "#"
+    },
+    {
+      title: "Sign out",
+      link: "#"
+    },
+  ]
+  
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <nav className = "flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src={logo} />
+        <div className = "flex gap-x-8">
+        <NavItems 
+          menu={menu} 
+          activeTab={activeTab} 
+          handleClick={setActiveTab}
+        />
+          {/* <Container>
+            <navItems menu={menu} />
+          </Container> */}
+        </div>
+      </nav>
     </>
   )
 }
---
+++ src/components/NavItems.jsx
@@ -0,0 +1,17 @@
+// destructuring js 
+function NavItems({ menu, activeTab, handleClick }) {
+    return (
+      <div className="flex gap-x-8">
+        {menu.map((item, index) => (
+          <a 
+            onClick={() => handleClick(item.title)} 
+            className={activeTab !== item.title ? "text-black" : "text[#19918F] font-bold"} 
+            key={index} 
+            href={item.link}>{item.title}
+        </a>
+        ))}
+      </div>
+    );
+  }
+  
+  export default NavItems;
