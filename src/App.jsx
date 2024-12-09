4469972e114d32b2b5865e84e04d1315a9c72c5c
putri afia <putriafianurfallahi@gmail.com>
2024-12-09 15:43:44 +0700

    create greeting text and user avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,13 @@
-import { useState } from "react"
-import logo from "./assets/WhatsApp Image 2024-12-09 at 11.23.54.jpeg"
-import NavItems from "./components/NavItems"
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 function App() {
-  const [activeTab, setActiveTab] = useState("dashboard")
-  const menu  = [
-    {
-      title: "Dashboard",
-      link: "#"
-    },
-    {
-      title: "Transfer",
-      link: "#"
-    },
-    {
-      title: "Topup",
-      link: "#"
-    },
-    {
-      title: "Sign out",
-      link: "#"
-    },
-  ]
-  
   return (
-    <>
-      <nav className = "flex items-center w-screen justify-between bg-white py-4 px-8">
-        <img src={logo} />
-        <div className = "flex gap-x-8">
-        <NavItems 
-          menu={menu} 
-          activeTab={activeTab} 
-          handleClick={setActiveTab}
-        />
-          {/* <Container>
-            <navItems menu={menu} />
-          </Container> */}
-        </div>
-      </nav>
-    </>
-  )
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,17 +1,28 @@
-// destructuring js 
-function NavItems({ menu, activeTab, handleClick }) {
-    return (
-      <div className="flex gap-x-8">
-        {menu.map((item, index) => (
-          <a 
-            onClick={() => handleClick(item.title)} 
-            className={activeTab !== item.title ? "text-black" : "text[#19918F] font-bold"} 
-            key={index} 
-            href={item.link}>{item.title}
-        </a>
-        ))}
-      </div>
-    );
-  }
-  
-  export default NavItems;
+import { useState } from "react";
+
+function NavItems({ menu }) {
+  const [activeTab, setActiveTab] = useState("Dashboard");
+
+  return (
+    <ul className="flex gap-x-8 text-black">
+      {menu.map((item) => {
+        return (
+          <a
+            key={item.title}
+            href={item.link}
+            className={`${
+              activeTab === item.title
+                ? "text-[#19918F] font-bold"
+                : "text-black"
+            }`}
+            onClick={() => setActiveTab(item.title)}
+          >
+            {item.title}
+          </a>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default NavItems;
